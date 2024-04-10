df547265d47a34c80417b43aa3429d5616dd351e
soloman-err <soloman.err@gmail.com>
2024-04-11 01:04:11 +0600

    vau initialized

---
+++ src/views/ContactView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contact">
+    <h1>Contact US</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .contact {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/DocView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="doc">
+    <h1>Documentation</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .doc {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
